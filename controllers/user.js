c65b25974b7c4b54318512b7f23b4dcdac2afb0c
khongphaiphuong <86140136+XuanPhuongsat@users.noreply.github.com>
2022-11-07 11:03:02 +0700

    userController
    dvbdfsew

--- controllers/user.js
+++ controllers/user.js
@@ -13,5 +13,28 @@ class userController{
     register_post(rep,res){
 
     }
+    forgot_get(req, res) { res.render('Login/forgot-password') };
+    async forgot_post(req, res) {
+                const { email } = req.body
+                const { password } = req.body
+                    //res.send(email)
+                let UserEmail = await AccountModel.findOne({ email })
+                let UserID = await AccountModel.findById('6367ca98d840d1daec4ab049')
+
+                let Userpass = AccountModel.findOne(password)
+                if (!UserEmail) {
+                    res.send('User not registered')
+                    return;
+                }
+                const secret = JWT_SECRET + Userpass.password
+                const payload = {
+                    email: UserEmail.email,
+                    _id: UserID._id
+                }
+                const token = jwt.sign(payload, secret, { expiresIn: '5m' })
+                const link = `http://localhost:3000/signup/reset-password/${UserID._id}/${token}`
+                console.log(link);
+                res.send('Mã reset mật khẩu đã được gửi vào email...')
+            }
 }
 module.exports = new userController;
