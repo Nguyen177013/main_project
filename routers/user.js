eaa6615c0c7c397637b5a9301aec12fe29bc85ea
khongphaiphuong <86140136+XuanPhuongsat@users.noreply.github.com>
2022-11-09 12:47:56 +0700

    update router
    adding get/post

--- routers/user.js
+++ routers/user.js
@@ -5,4 +5,8 @@ router.get('/login',user.login_get);
 
 router.get('/register',user.register_get);
 router.post('/register',user.register_post);
+router.get('/forgot-password', user.forgot_get);
+router.post('/forgot-password', user.forgot_post);
+router.get('/reset-password/:_id/:token', user.reset_get);
+router.post('/reset-password/:_id/:token', user.reset_post);
 module.exports = router;
